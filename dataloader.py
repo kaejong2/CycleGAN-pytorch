b539bf66325ba14d5ed5aaf3b42a312d6adf7a97
kaejong2 <kaejong2@gmail.com>
2020-11-26 19:21:01 +0900

    1126

--- dataloader.py
+++ dataloader.py
@@ -2,19 +2,22 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
-# Image processing
-transform = transforms.Compose([
-                transforms.Resize(64),
-                transforms.ToTensor(),
-                transforms.Normalize((0.5,),(0.5,))])
+def data_loader(args):
+    # Image processing
+    transform = transforms.Compose([
+                    transforms.Resize(args.image_size),
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5,),(0.5,))])
 
-# MNIST dataset
-mnist = torchvision.datasets.MNIST(root='./data/',
-                                   train=True,
-                                   transform=transform,
-                                   download=True)
+    # MNIST dataset
+    mnist = torchvision.datasets.MNIST(root=args.root_path,
+                                    train=True,
+                                    transform=transform,
+                                    download=True)
 
-# Data loader
-data_loader = torch.utils.data.DataLoader(dataset=mnist,
-                                          batch_size=32, 
-                                          shuffle=True)
+    # Data loader
+    data_loader = torch.utils.data.DataLoader(dataset=mnist,
+                                            batch_size=args.batch_size, 
+                                            shuffle=True)
+
+    return data_loader
